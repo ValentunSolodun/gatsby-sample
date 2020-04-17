dd837a461f957ab1d9253777b296bd45c12fd4cb
ValentunSolodun <valentun.solodun@gmail.com>
2020-04-17 11:57:12 +0300

    Some fix

--- src/templates/all-todo.js
+++ src/templates/all-todo.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import { Card } from "semantic-ui-react"
 import Layout from "../components/layout"
+import { useMount } from "@umijs/hooks"
+import { getAllTodo } from "../actions"
 
-export default ({ pageContext: { item } }) => {
+export default ({pageContext: {item}}) => {
   return (
     <Layout withoutHeader={true}>
       <Card>
         <Card.Content>
-          <Card.Header>{item.text}</Card.Header>
+          <Card.Header>{item.name}</Card.Header>
         </Card.Content>
       </Card>
     </Layout>
